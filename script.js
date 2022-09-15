28c0bb8d42ab70551777988e4751037b49e4804d
Reznik Maksim <108272491+MaksPik4erz@users.noreply.github.com>
2022-09-15 16:48:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
 function calculateWordsInText(str) {
     let arr = [];
     let word = '';
-
+    
     for (let i = 0; i <= str.length; i++) {
 
         if ( str[i] === ' ' || str.length === i ) {
-
-            
             arr.push(word);
             word = ' ';
         } else {
-
             word += str[i];
-          
         };
         
     }
